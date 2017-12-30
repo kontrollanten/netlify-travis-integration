762cf3c9b9325d918e77d17a384d7bbe495add05
kontrollanten <alexander@width.se>
2017-12-30 19:11:12 +0000

    add eslint-plugin-ava

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
-    "extends": "airbnb-base",
+    "extends": [
+      "airbnb-base",
+      "plugin:ava/recommended"
+    ],
     "rules": {
       "no-console": [0],
-    }
+    },
+    "plugins": [
+      "ava"
+    ]
 };
