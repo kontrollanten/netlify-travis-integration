3598307bfccc11feb862c98cb2ee888efdb375cb
kontrollanten <alexander@width.se>
2017-12-31 05:36:21 +0000

    add eslint-plugin-node

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,15 @@
 module.exports = {
     "extends": [
       "airbnb-base",
-      "plugin:ava/recommended"
+      "plugin:ava/recommended",
+      "plugin:node/recommended"
     ],
     "rules": {
       "no-console": [0],
+      "node/no-unpublished-require": 0,
     },
     "plugins": [
-      "ava"
+      "ava",
+      "node"
     ]
 };
